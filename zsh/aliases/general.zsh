0cf841bf571f3a44416d75e19da91a557871c857
julienmiclo <julien@billiotte.fr>
2021-01-21 18:14:04 +0100

    update aliases

--- zsh/aliases/general.zsh
+++ zsh/aliases/general.zsh
@@ -3,3 +3,25 @@ alias ld="ls"
 alias ll="ls -lah --color=auto --group-directories-first"
 alias l="ls -CF"
 alias c="clear"
+
+alias mv="mv -i"
+for c in cp rm chmod chown rename; do
+	alias $c="$c -v"
+done
+
+alias v="vim -R -"
+for i in /usr/share/vim/vim*/macros/less.sh(N); do
+	alias v="$i"
+done
+
+alias home="cd ~"
+alias dotfiles="cd ~/.dotfiles"
+
+alias weather="curl wttr.in/nancy"
+
+alias whois="whois -h geektools.com"
+alias scp="scp -C -p"
+alias screen="screen -U"
+alias x="screen -A -x"
+alias r="screen -D -R"
+
