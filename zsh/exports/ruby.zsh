d574bd1317fb8b2a379029a97cc133b37c66637c
julienmiclo <julien@billiotte.fr>
2021-01-21 18:14:23 +0100

    update exports

---
+++ zsh/exports/ruby.zsh
@@ -0,0 +1,4 @@
+export PATH=/opt/homebrew/opt/ruby/bin:$PATH
+export LDFLAGS="-L/opt/homebrew/opt/ruby/lib"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/ruby/lib/pkgconfig"
+export CPPFLAGS="-I/opt/homebrew/opt/ruby/include"
