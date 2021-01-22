719b75517506e9620718d438a3961c16621fb5d2
julienmiclo <julien@billiotte.fr>
2021-01-22 22:28:25 +0100

    update aliases,exports

--- zsh/aliases/server.zsh
+++ zsh/aliases/server.zsh
@@ -1,4 +1,7 @@
-alias check-server="apachectl configtest && brew services"
-alias restart-server="brew services restart httpd && brew services restart php && brew services restart mysql && brew services"
-alias start-server="brew services start httpd && brew services start php && brew services start mysql"
-alias stop-server="brew services stop php && brew services stop httpd && brew services stop mysql"
+alias server-check="apachectl configtest && brew services"
+alias server-restart="brew services restart httpd && brew services restart php && brew services"
+alias server-start="brew services start httpd && brew services start php"
+alias server-stop="brew services stop php && brew services stop httpd"
+
+alias php74="sh ~/.scripts/phpswitcher.sh 7.4"
+alias php8="sh ~/.scripts/phpswitcher.sh 8.0"
--- zsh/exports/ruby.zsh
+++ zsh/exports/ruby.zsh
@@ -2,3 +2,4 @@ export PATH=/opt/homebrew/opt/ruby/bin:$PATH
 export LDFLAGS="-L/opt/homebrew/opt/ruby/lib"
 export PKG_CONFIG_PATH="/opt/homebrew/opt/ruby/lib/pkgconfig"
 export CPPFLAGS="-I/opt/homebrew/opt/ruby/include"
+export PATH=/opt/homebrew/lib/ruby/gems/3.0.0/bin:$PATH
