0c2431f65da1244da38df66f69e6a71a79b7a047
Julien Miclo <julienmiclo@gmail.com>
2021-02-19 10:11:04 +0100

    add android,java exports

---
+++ zsh/exports/android.zsh
@@ -0,0 +1,5 @@
+export ANDROID_SDK_ROOT=~/WebServer/android/sdk
+
+export PATH=$PATH:$ANDROID_SDK_ROOT:$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/platform-tools:$ANDROID_SDK_ROOT/build-tools:$ANDROID_SDK_ROOT/platforms
+
+export PATH=$PATH:~/.gradle
