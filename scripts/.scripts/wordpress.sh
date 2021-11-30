4063734bf5c5d24276b55d707e17b59afe5d6000
Julien Miclo <julienmiclo@gmail.com>
2021-11-30 12:31:31 +0100

    update(scripts): add wordpress script

---
+++ scripts/.scripts/wordpress.sh
@@ -0,0 +1,6 @@
+wp core download --locale=fr_FR
+
+wp config create --dbname=bc_wp_cncb --dbuser=root --dbpass=root --locale=fr_FR
+
+wp core install --url=localhost/repositories/cncb --title=CNCB --admin_user=adminbilliotte --admin_password=gerard55-cncb --admin_email=web@billiotte.fr
+
