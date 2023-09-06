0decb5b2d58d7c529c8b422aa1e8437f9978464c
Julien Miclo <julienmiclo@gmail.com>
2023-09-06 15:48:41 +0200

    update(): add php 7.4 and php 5.6 support

--- scripts/.scripts/phpswitcher.sh
+++ scripts/.scripts/phpswitcher.sh
@@ -6,7 +6,8 @@ apache_conf_path="/Users/julienmiclo/.dotfiles/httpd/custom/001-loadmodules.conf
 
 php_version="php"
 php_module="php_module"
-apache_php_lib_path="/lib/httpd/modules/libphp.so"
+module_name="libphp"
+#apache_php_lib_path="/lib/httpd/modules/libphp.so"
 
 if [ "$1" == "8.2" ]
 then
@@ -14,9 +15,24 @@ then
 elif [ "$1" == "8.1" ]
 then
         php_version="php@8.1"
+elif [ "$1" == "8.0" ]
+then
+	php_version="php@8.0"
+elif [ "$1" == "7.4" ]
+then
+	php_module="php7_module"
+	php_version="php@7.4"
+	module_name="libphp7"
+elif [ "$1" == "5.6" ]
+then
+	php_module="php5_module"
+	php_version="php@5.6"
+	module_name="libphp5"
 fi
 
-apache_php_mod_path="/opt/homebrew/opt/$php_version/lib/httpd/modules/libphp.so"
+
+apache_php_mod_path="/opt/homebrew/opt/$php_version/lib/httpd/modules/$module_name.so"
+
 
 for v in `ls /opt/homebrew/opt/ | grep php | cut -c4-8 | uniq`; do
 	brew unlink php$v > /dev/null
@@ -28,7 +44,7 @@ php -v
 sed -i '' -e '$ d' $apache_conf_path
 echo "LoadModule $php_module $apache_php_mod_path" >> $apache_conf_path
 
-brew services restart httpd && brew services restart php
+brew services restart httpd -d -v && brew services restart php -d -v
 
 brew services
 
